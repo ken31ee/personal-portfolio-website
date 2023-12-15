f45fa1700427e74c65ba33dedc4821f68cac5ed4
Ken Tung <tungpakyin04@outlook.com>
2023-12-14 17:14:58 -0800

    chore: add prettifier

---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  plugins: ["prettier-plugin-tailwindcss"],
+  "editor.formatOnSave": false,
+    // Enable per-language
+    "[javascript]": {
+        "editor.formatOnSave": true
+    }
+};
